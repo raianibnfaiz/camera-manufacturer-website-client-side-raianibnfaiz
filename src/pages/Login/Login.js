f341643acbdbd6c0ccad4ba777f8405556b3dd1e
Raian Ibn Faiz <raianibnfaiz@gmail.com>
2022-07-22 12:16:41 +0600

    second commit

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <button class="btn btn-primary">Button</button>
+        </div>
+    );
+};
+
+export default Login;
